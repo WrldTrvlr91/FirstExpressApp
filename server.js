469d69b22362385fcab8008f14868a4617758dde
Rob Sonter <rob.sonter@newstartdigital.com.au>
2020-11-15 14:54:03 +0800

    app - POST, DELETE and GET functioning

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+
 
 // Sets up the Express App
 // =============================================================
@@ -11,23 +14,73 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"))
 
 // Routes
 // =============================================================
 
-app.get("/", function(req, res){
-res.sendFile(path.join(__dirname, "assets/html/index.html"));
-console.log("I AM HOME");
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    console.log("NOTES HERE I AM");
 });
 
-app.get("/notes", function(req, res){
-res.sendFile(path.join(__dirname, "assets/html/notes.html"));
-console.log("NOTES HERE I AM");
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+    console.log("I AM HOME *");
+});
+
+// Returns all notes
+app.get("/api/notes", function (req, res) {
+    let allItems = JSON.parse(fs.readFileSync("./db.json"))
+    return res.json(allItems);
+    
+});
+
+//Create new note
+app.post("/api/notes", function (req, res) {
+
+    let newNote = req.body;
+
+    newNote.id = uuidv4();
+
+    res.json(newNote);
+
+    fs.readFile("./db.json", function (err, data) {
+        if (err) throw err;
+        let jsonOutput = JSON.parse(data);
+        jsonOutput.push(newNote);
+
+        fs.writeFile("./db.json", JSON.stringify(jsonOutput), function (err) {
+            if (err) throw err;
+        
+        });
+    });
+
+});
+
+// Delete Note
+app.delete("/api/notes/:id", function (req, res) {
+    let selectedNote = req.params.id
+
+    res.json(selectedNote);
+
+    fs.readFile("./db.json", function (err, data) {
+        if (err) throw err;
+        let jsonOutput = JSON.parse(data);
+        for (var i = 0; i < jsonOutput.length; i++)
+            if (jsonOutput[i].id === selectedNote) {
+                jsonOutput.splice(i, 1);
+                break;
+            }
+            fs.writeFile("./db.json", JSON.stringify(jsonOutput), function (err) {
+                if (err) throw err;
+
+        });
+    })
 });
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
